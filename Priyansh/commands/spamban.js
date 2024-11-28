12dbf09da6c93cd22725203280ec1e0442c6d6a1
Raj <159320744+Nobita-789@users.noreply.github.com>
2024-11-28 18:50:30 +0530

    Update spamban.js

--- Priyansh/commands/spamban.js
+++ Priyansh/commands/spamban.js
@@ -1,4 +1,4 @@
- const num = 10 //number of times spam gets banned -1, for example 5 times 6 times will get banned
+const num = 10 //number of times spam gets banned -1, for example 5 times 6 times will get banned
 const timee = 120 // During `timee` spam `num` times will be banned
  module.exports.config = {
   name: "spamban",
@@ -18,20 +18,20 @@ module.exports. run = async function ({api, event})  {
 module.exports.handleEvent = async function ({ Users, Threads, api, event})  {
   let { senderID, messageID, threadID } = event;
   var datathread = (await Threads.getData(event.threadID)).threadInfo;
-  
+
   if (!global.client.autoban) global.client.autoban = {};
-  
+
   if (!global.client.autoban[senderID]) {
     global.client.autoban[senderID] = {
       timeStart: Date.now(),
       number: 0
     }
   };
-  
+
   const threadSetting = global.data.threadData.get(threadID) || {};
   const prefix = threadSetting.PREFIX || global.config.PREFIX;
   if (!event.body || event.body.indexOf(prefix) != 0) return;
-  
+
   if ((global.client.autoban[senderID].timeStart + (timee*1000)) <= Date.now()) {
     global.client.autoban[senderID] = {
       timeStart: Date.now(),
@@ -56,7 +56,7 @@ module.exports.handleEvent = async function ({ Users, Threads, api, event})  {
         timeStart: Date.now(),
         number: 0
       };
-      api.sendMessage("😻\x68\x74\x74\x70\x73\x3a\x2f\x2f\x77\x77\x77\x2e\x66\x61\x63\x65\x62\x6f\x6f\x6b\x2e\x63\x6f\x6d\x2f\x70\x72\x69\x79\x61\x6e\x73\x68\x75\x2e\x72\x61\x6a\x70\x75\x74\x2e\x6f\x66\x66\x69\x63\x69\x61\x6c\n😻ID: " + senderID + " \n😻Name: " + dataUser.name + `\n😻Reason: spam bot ${num} time/${timee}s\n\n✔️Reported to admin bot`, threadID,
+      api.sendMessage("🥲Aur Karle spam 😏\n😻ID: " + senderID + " \n😻Name: " + dataUser.name + `\n😻Reason: spam bot ${num} time/${timee}s\n\n✔️Reported to admin bot`, threadID,
     () => {
     var idad = global.config.ADMINBOT;
     for(let ad of idad) {
